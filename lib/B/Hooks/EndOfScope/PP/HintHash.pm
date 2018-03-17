60ab38eccab06b0853e18c8d372f2b08ff6c1088
Peter Rabbitson <ribasushi@leporine.io>
2018-03-17 21:18:37 +0100

    Fix incorrect merge - $] *must* be referred to via a constant
    Consider:
    
      ~$ perl -e '
    
        use warnings;
        use strict;
    
        use constant _PERL_VERSION => "$]";
    
        sub with_const {
          warn "stuff" if _PERL_VERSION > 5.006;
        }
    
        sub without_const {
          warn "stuff" if "$]" > 5.006;
        }
    
        use Data::Dumper;
        $Data::Dumper::Deparse = 1;
        warn Dumper [ \&with_const, \&without_const ];
      '
    
    This reverts commit a2ff0a6617f0262d1512a70a2d46b4fe916cb0ec, and adds
    an extra constant/guard to not leak an array when we don't need to.

--- lib/B/Hooks/EndOfScope/PP/HintHash.pm
+++ lib/B/Hooks/EndOfScope/PP/HintHash.pm
@@ -11,6 +11,12 @@ use warnings;
 our $VERSION = '0.23';
 
 use Scalar::Util ();
+use constant _NEEDS_MEMORY_CORRUPTION_FIXUP => (
+  "$]" >= 5.008
+    and
+  "$]" < 5.008004
+) ? 1 : 0;
+
 
 # This is the original implementation, which sadly is broken
 # on perl 5.10+ within string evals
@@ -18,19 +24,17 @@ sub on_scope_end (&) {
 
   # the scope-implicit %^H localization is a 5.8+ feature
   $^H |= 0x020000
-    if "$]" >= 5.008;
+    unless B::Hooks::EndOfScope::PP::_PERL_VERSION < 5.008;
 
   # the explicit localization of %^H works on anything < 5.10
   # but we use it only on 5.6 where fiddling $^H has no effect
   local %^H = %^H
-    if "$]" < 5.008;
+    if B::Hooks::EndOfScope::PP::_PERL_VERSION < 5.008;
 
-  # Workaround for memory corruption dueing implicit $^H-induced
+  # Workaround for memory corruption during implicit $^H-induced
   # localization of %^H on 5.8.0~5.8.3, see extended comment below
   bless \%^H, 'B::Hooks::EndOfScope::PP::HintHash::__GraveyardTransport' if (
-    "$]" >= 5.008
-      and
-    "$]" < 5.008004
+    _NEEDS_MEMORY_CORRUPTION_FIXUP
       and
     ref \%^H eq 'HASH'  # only bless if it is a "pure hash" to start with
   );
@@ -68,7 +72,8 @@ sub B::Hooks::EndOfScope::PP::_SG_STACK::DESTROY {
 
   # "Leak" this entire structure: ensures it and its contents will not be
   # garbage collected until the very very very end
-  push @Hint_Hash_Graveyard, \@Hint_Hash_Graveyard;
+  push @Hint_Hash_Graveyard, \@Hint_Hash_Graveyard
+    if _NEEDS_MEMORY_CORRUPTION_FIXUP;
 
   sub B::Hooks::EndOfScope::PP::HintHash::__GraveyardTransport::DESTROY {
 
